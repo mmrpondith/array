41b4f22f7f13e18d2719487e076b395014a58e67
“rahulislamitibd” <itibdd@gmail.com>
2021-02-12 18:35:59 +0600

    array test

---
+++ test.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+var students [5]string 
+students [0]="Siam"
+students [1]="rahat"
+students [2]="Salam"
+students [3]="Najnin"
+students [4]="vairob"
+fmt.Println(students)
+fmt.Println(students [4])
+fmt.Println(len(students))
+}
